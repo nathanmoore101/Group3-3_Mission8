e3ecacd60cab4c0a7441e80caf1695572d32578e
unknown <alliemarshall99@gmail.com>
2024-02-26 20:31:37 -0800

    Models made - database not made

---
+++ Group3-3_Mission8/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group3_3_Mission8.Models
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Group3-3_Mission8/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group3_3_Mission8.Models
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
