8ba4081706ed0b1e046b7d1f2131e5f55cf2302a
unknown <alliemarshall99@gmail.com>
2024-03-01 17:18:35 -0800

    Repo Interface added

---
+++ Group3-3_Mission8/Models/EFDataRepo.cs
@@ -0,0 +1,24 @@
+﻿namespace Group3_3_Mission8.Models
+{
+    public class EFDataRepo : IDataRepo
+    {
+        private TaskDbContext _context;
+        public EFDataRepo(TaskDbContext temp)
+
+        {  _context = temp; }
+
+        public List<TaskModel> Tasks => _context.Tasks.ToList();
+
+        public void AddTask(TaskModel task) 
+        
+        { _context.Add(task);
+          _context.SaveChanges();
+        }
+
+        public void DeleteTask(TaskModel task)
+        {
+            _context.Remove(task);
+            _context.SaveChanges();
+        }
+    }
+}
